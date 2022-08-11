6b44e3817198e1561db2ec5f4628d1679d2f9c71
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-11 23:19:33 +0900

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello() {
-        return "please come comeasdasdasdasd comcomcocmasdasdasdasdaomoc";
+        return "please come";
     }
 }
