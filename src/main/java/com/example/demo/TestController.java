b701128238aa8f545483933356244f7bee4ed794
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-11 15:08:40 +0900

    hello jenkins

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "hello jenkins nice to meet you!";
     }
 }
